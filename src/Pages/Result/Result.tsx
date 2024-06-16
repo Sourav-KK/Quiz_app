695152cb458e860bb73509b241dc775d56436044
Sourav <souravunofficial000@gmail.com>
2024-06-16 12:18:10 +0530

    updated basic stying of result  page

--- src/Pages/Result/Result.tsx
+++ src/Pages/Result/Result.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+import "../../Styles/result.css";
 
 const Result = () => {
   return (
-    <div>Result page</div>
-  )
-}
+    <div className="result-holder">
+      <div className="imoji-holder">
+        <img src="" alt="" />
+        <p className="score-comment">Better luck next time</p>
+      </div>
+      <div className="result-summary">
+        <h4 className="final-result">You score is : 8/10</h4>
+        <h4 className="final-result">You attempted : 8 questions</h4>
+        <h4 className="final-result">Wrong answers : 8 </h4>
+      </div>
 
-export default Result
+      <div className="return-btn-holder">
+        <button type="button">
+          <Link to={"/"}>Home</Link>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Result;
