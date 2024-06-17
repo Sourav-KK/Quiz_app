9563634fa7f68a2169f170abe56c24a6694dc7dc
Sourav <souravunofficial000@gmail.com>
2024-06-17 07:59:17 +0530

    score and message displayed

--- src/Pages/Result/Result.tsx
+++ src/Pages/Result/Result.tsx
@@ -1,17 +1,20 @@
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import "../../Styles/result.css";
+import { RootState } from "../../Utilities/Redux/Store";
 
 const Result = () => {
+  const totScore = useSelector((state: RootState) => state.Answers.totalScore);
+  const message = useSelector((state: RootState) => state.Answers.message);
+
   return (
     <div className="result-holder">
       <div className="imoji-holder">
         <img src="" alt="" />
-        <p className="score-comment">Better luck next time</p>
+        <p className="score-comment">{message}</p>
       </div>
       <div className="result-summary">
-        <h4 className="final-result">You score is : 8/10</h4>
-        <h4 className="final-result">You attempted : 8 questions</h4>
-        <h4 className="final-result">Wrong answers : 8 </h4>
+        <h4 className="final-result">You score is : {totScore}/10</h4>
       </div>
 
       <div className="return-btn-holder">
